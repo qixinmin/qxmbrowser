a5b82c4e99613d415894d000669dd25220a13cb3
qixm <qixm@2345.com>
2017-03-15 17:50:02 +0800

    增加编译到release的apk

--- app/src/main/java/com/qxm/qxmbrowser/activity/BrowserActivity.java
+++ app/src/main/java/com/qxm/qxmbrowser/activity/BrowserActivity.java
@@ -4,52 +4,83 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.qxm.qxmbrowser.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TODO 1. 清除界面，仿照作出滑屏组件和下滑组件，左右组件，等
 public class BrowserActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerViewCenter;
+
+    private List<String> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initData();
+        mRecyclerViewCenter = (RecyclerView) findViewById(R.id.recycler_view_center);
+        mRecyclerViewCenter.setLayoutManager(new GridLayoutManager(this, 4));
+        mRecyclerViewCenter.setAdapter(new HomeAdapter());
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_browser, menu);
-        return true;
+    private void initData() {
+        mData = new ArrayList<>();
+        for(int i ='A'; i< 'Z' ; ++i ) {
+            mData.add(String.valueOf(i));
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
+    {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    BrowserActivity.this).inflate(R.layout.homeitem, parent,
+                    false));
+            return holder;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position)
+        {
+            holder.tv.setText(mData.get(position));
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mData.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder
+        {
+
+            TextView tv;
+
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.list_item);
+            }
+        }
     }
 }
