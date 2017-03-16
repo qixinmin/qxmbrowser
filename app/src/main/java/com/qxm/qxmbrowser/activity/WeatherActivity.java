98f16fc510aec2068478fa3aff5e241169f35a88
qixm <qixm@2345.com>
2017-03-16 13:48:34 +0800

    状态栏透明效果

--- app/src/main/java/com/qxm/qxmbrowser/activity/WeatherActivity.java
+++ app/src/main/java/com/qxm/qxmbrowser/activity/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.qxm.qxmbrowser.activity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -48,6 +50,15 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //状态栏透明效果
+        if(Build.VERSION.SDK_INT >= 21) {//5.0
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
 
         bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
